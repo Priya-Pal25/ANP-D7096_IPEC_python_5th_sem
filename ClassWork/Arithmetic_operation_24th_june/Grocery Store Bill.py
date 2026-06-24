724ea65ed05523729611416c5744f747842a814f
Priya-Pal25 <priyapal2509@gmail.com>
2026-06-24 12:00:05 +0530

    Initial commit

---
+++ ClassWork/Arithmetic_operation_24th_june/Grocery Store Bill.py
@@ -0,0 +1,6 @@
+"""Write a Python program to calculate the total cost of rice packets purchased. """
+
+Price = float(input("Enter the Price per packet:"))
+Number_of_Packets = int(input("Enter the number of packets:"))
+
+print("Total Bill Amount : ",(Price * Number_of_Packets))
---
+++ ClassWork/Arithmetic_operation_24th_june/Monthly Salary Calculation.py
@@ -0,0 +1,7 @@
+""" Write a Python program to calculate the total monthly salary of an employee by adding the fixed salary 
+and incentive amount. """
+
+Basic_Salary = float(input("Enter the Basic Salary:"))
+incentive = float(input("Enter the Incentive amount:"))
+
+print("Total Salary :",(Basic_Salary + incentive))
