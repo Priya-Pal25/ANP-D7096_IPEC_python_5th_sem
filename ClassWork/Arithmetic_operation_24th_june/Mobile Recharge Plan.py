6dd9ad329fe99f7a98290db94d0a8430c0fe9d01
Priya-Pal25 <priyapal2509@gmail.com>
2026-06-25 21:11:59 +0530

    Program to find Mobile Recharg plan

--- ClassWork/Arithmetic_operation_24th_june/Mobile Recharge Plan.py
+++ ClassWork/Arithmetic_operation_24th_june/Mobile Recharge Plan.py
@@ -1,6 +1,25 @@
 '''Write a Python program to calculate the total recharge amount based on the data pack selected.'''
 
+# Taking input from the user
+
 cost = float(input("Enter the Cost per GB : "))
-number_of_gbs =  int(input ("Enter number of GBs : "))
+number_of_gbs =  float(input ("Enter number of GBs : "))
+#---------------------------------------------------------------------------------------------------------
+
+# Displaying the input to the user
+
+print("Cost per GB : " ,cost)
+print("Number of Gbs : ",number_of_gbs)
+#---------------------------------------------------------------------------------------------------------
+
+# Displaying the Total Recharge Cost
 
 print("Total Recharge Cost : ",(cost * number_of_gbs))
+#----------------------------------------------------------------------------------------------------------
+
+'''Output:
+Enter the Cost per GB : 85.0
+Enter number of GBs : 1.5 
+Cost per GB :  85.0
+Number of Gbs :  1.5
+'''
