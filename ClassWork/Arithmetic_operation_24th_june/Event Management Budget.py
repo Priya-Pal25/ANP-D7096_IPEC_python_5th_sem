eb4cba7cdb0f841865d9bd42c645adb424b15311
Priya-Pal25 <priyapal2509@gmail.com>
2026-06-25 20:18:19 +0530

    Program to find Event management Budget

--- ClassWork/Arithmetic_operation_24th_june/Event Management Budget.py
+++ ClassWork/Arithmetic_operation_24th_june/Event Management Budget.py
@@ -1,6 +1,25 @@
 '''Write a Python program to calculate how much each participant should pay. '''
+#takng input from user
 
-cost = float(input("Enter the Total Event Cost : "))
+cost = float(input("Enter the Total Event Cost (in Rs): "))
 participants = int(input("Enter the Number of participants : "))
 
+#----------------------------------------------------------------------------------------
+
+#display the input to the user
+print("Total Event Cost (in Rs): " ,cost)
+print("Number of Participant : ",participants)
+
+#----------------------------------------------------------------------------------------
+
+#displaying Amount per participants
+
 print("Amount per Participant : ",(cost / participants))
+#---------------------------------------------------------------------------------------
+'''Output:
+Enter the Total Event Cost (in Rs): 500.0
+Enter the Number of participants : 10
+Total Event Cost (in Rs):  500.0
+Number of Participant :  10
+Amount per Participant :  50.0
+'''
