7e71d0bd788730555d0c6e9d9442e6af6ef27a4b
Priya-Pal25 <priyapal2509@gmail.com>
2026-06-25 20:39:46 +0530

    Program to find Classroom Seating Arrangement.py

--- ClassWork/Arithmetic_operation_24th_june/Classroom Seating Arrangement .py
+++ ClassWork/Arithmetic_operation_24th_june/Classroom Seating Arrangement .py
@@ -1,6 +1,22 @@
-"""Write a Python program to determine how many complete rows can be formed."""
+'''Write a Python program to determine how many complete rows can be formed.'''
+#taking input from the user
 
 Total_Student = int(input("Enter the Total Students : " ))
 Student_per_row = int(input("Enter the number of Student per rows : " ))
+#------------------------------------------------------------------------------------------
+
+#displaying the input to the user
+print("Total Students : ",Total_Student)
+print("Student per Row : ",Student_per_row)
+#-------------------------------------------------------------------------------------------
+
+#displaying the Number of complete Rows
 
 print("Number of Complete Rows : " , (Total_Student / Student_per_row))
+#--------------------------------------------------------------------------------------------
+'''Output :
+Enter the Total Students : 60
+Enter the number of Student per rows : 5
+Total Students :  60
+Student per Row :  5
+Number of Complete Rows :  12.0'''
