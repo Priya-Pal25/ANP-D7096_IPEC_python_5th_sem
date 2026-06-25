01147dec72a22cdae67042df3d5952330a95c313
Priya-Pal25 <priyapal2509@gmail.com>
2026-06-25 11:57:43 +0530

    program to find speed

---
+++ ClassWork/selection_statement_25th_june/day 1/speed.py
@@ -0,0 +1,18 @@
+'''Write a program to calculate speed when distance and time has been po=rovided'''
+
+#taking input from user
+
+distance = float(input("Enter the distance (in km): "))
+time = float(input("Enter the time : "))
+
+#display the distance and time
+
+print("Distance : " ,distance)
+print("speed : " ,time)
+
+#calculate speed
+print("Speed : " ,(distance/time))
+
+
+
+
