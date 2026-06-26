5eaf6abbeb415f1e0cc39a06c820f933c35b0d1d
Priya-Pal25 <priyapal2509@gmail.com>
2026-06-26 11:13:38 +0530

    Program to check whether a number is a natural number or not

---
+++ ClassWork/day 3 selection_statement_26th_june/natural_number.py
@@ -0,0 +1,24 @@
+'''Write a program to check whether a number is a natural number or not'''
+
+#--------------------------------------------------------------------
+#-----------------------Coding----------------------------------------
+
+# Taking number from the user
+
+num = int(input("Enter number: "))
+
+#-----------------------------------------------------------------------
+
+# checking whether the number is natural number or not
+
+if (num > 0):
+    print(num ,"is a Natural number")
+else:
+    print(num ,"is not a natural number")
+
+#------------------------------------------------------------------------
+
+'''Output:
+Enter number: 5
+5 is a Natural number
+'''
