e5083a4741fb8eaca43fca1fe67d5565f2ee0785
Priya-Pal25 <priyapal2509@gmail.com>
2026-06-27 10:02:12 +0530

    Program to check mobile battery

---
+++ ClassWork/day 4 selection_statement_practice/mobile_battery_warning.py
@@ -0,0 +1,48 @@
+'''----------------------------------- Mobile Battery Warning ------------------------------------------------
+
+A smartphone displays a low battery warning only when the battery percentage falls below 15%. 
+Write a Python program to accept the battery percentage. 
+If the battery is below 15, display: 
+Connect Charger Immediately 
+Otherwise, display nothing. 
+-------------------------------------------------------------------------------------------------------------
+Sample Input 
+10 
+-------------------------------------------------------------------------------------------------------------
+Sample Output 
+Connect Charger Immediately
+-------------------------------------------------------------------------------------------------------------
+'''
+#------------------------------------------------------------------------------------------------------------
+#-------------------------------------------Coding-----------------------------------------------------------
+
+# Taking battery percentage input from the user
+
+battery_percentage = float(input("Enter the Battery Percentage : "))
+
+print("------------------------------------------------------------------------------------------------------")
+
+#-------------------------------------------------------------------------------------------------------------
+
+# Validating battery percentage
+
+if (battery_percentage <= 0):
+    exit("Battery Percentage must be positive")
+
+#------------------------------------------------------------------------------------------------------------------
+
+# Checking battery percentage
+
+if (battery_percentage < 15):
+    print("Connect Charger Immediately")
+
+else:
+    print("Nothing")
+
+#----------------------------------------------------------------------------------------------------------------
+
+'''Output:
+Enter the Battery Percentage : 50
+------------------------------------------------------------------------------------------------------
+Nothing
+'''
