77f7274de48ec517cbc485bf80051e886cb4a67e
Priya-Pal25 <priyapal2509@gmail.com>
2026-06-29 21:47:55 +0530

    Program to validate login credential

--- labWork/lab1 selection_statement_26th_june/cybersecurity_login_validation.py
+++ labWork/lab1 selection_statement_26th_june/cybersecurity_login_validation.py
@@ -25,9 +25,14 @@ Welcome Admin
 #------------------------------------------------------------------------------------------------------------
 #-----------------------------------------Coding-------------------------------------------------------------
 
+# correct credential
+correct_username = "admin"
+correct_password = "admin123"
+correct_otp = 4567
+
 # Taking input from the user
 username = input("Enter Username : ")
-income= input("Enter Password : ")
+password= input("Enter Password : ")
 otp= int(input("Enter four digit OTP : "))
 
 print("--------------------------------------------------------------------------------------")
@@ -39,5 +44,37 @@ if (otp <= 0):
 
 #----------------------------------------------------------------------------------------------------------
 
-if (username == 'admin'):
+if (username == correct_username and password == correct_password and otp == correct_otp):
+    print("Login Successful")
+elif (otp != correct_otp):
+    print("Re-Enter otp")
+elif (username != correct_username):
+    print("User Not Found")
+
+   
+else:
+    for x in range(1,4):
+        password = input("Enter Password: ")
+
+        
+        if password == correct_password:
+            break 
+
+        if x == 3:
+            print("Account Locked")
+            break
+
+        
+        print("Incorrect Password. Try Again.")
+
+#-------------------------------------------------------------------------------------------------------------------------
+
+'''Output:
+Enter Username : admin
+Enter Password : admin123
+Enter four digit OTP : 4567
+--------------------------------------------------------------------------------------
+Login Successful
+'''
+
     
