d5ff477c214278eae5695d76dda500fab6724163
Priya-Pal25 <priyapal2509@gmail.com>
2026-07-08 22:02:43 +0530

    Module

---
+++ labWork/lab4 module_8th_july/twodfigures.py
@@ -0,0 +1,46 @@
+# function to calculate area of a triangle
+def calculate_triangle_area(base,height):
+    return (0.5 * base * height)
+#----------------------------------------------------------------------------
+
+# function to calculate perimeter of a triangle
+def calcualte_triangle_perimeter(side1,side2,side3):
+    return (side1 + side2 + side3)
+
+#----------------------------------------------------------------------------
+
+# function to calculate the area of a circle
+def calculate_circle_area(radius):
+    return (3.14 * radius * radius)
+
+#-----------------------------------------------------------------------------
+
+# function to calculate the perimeter of a circle
+def calculate_circle_perimeter(radius):
+    return (2 * 3.14 * radius)
+
+#------------------------------------------------------------------------------
+
+# function to calculate the area of a square
+def calculate_square_area(side):
+    return (side * side )
+
+#------------------------------------------------------------------------------
+
+# function to calculation the perimeter of a square
+def calculate_square_perimeter(side):
+    return (4 * side)
+
+#------------------------------------------------------------------------------
+
+# function to calculate the area of a rectangle
+def calculate_rectangle_area(length,breadth):
+    return (length * breadth)
+
+#------------------------------------------------------------------------------
+
+# function to calculate the perimeter of a rectangle
+def calculate_rectangle_perimeter(length , breadth):
+    return (2 * (length + breadth))
+
+#-------------------------------------------------------------------------------
